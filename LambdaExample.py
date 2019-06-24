f103d3ec1b173c6555413e442d91f44ef979c1f4
Mystified131 <mystifiedthomas@gmail.com>
2019-06-24 12:45:20 -0500

    Adding functions

--- LambdaExample.py
+++ LambdaExample.py
@@ -5,3 +5,13 @@ print(x(5))
 y = lambda a : a * a * a
 print(y(3))
 
+z = lambda f, g : f in g
+print(z("rl", "world!"))
+
+examps = [1, 1, 3, 6, 5, 11]
+
+map(lambda m : m * 3, examps)
+
+s = lambda x: x.startswith('B')
+print(s('Bob'))
+
